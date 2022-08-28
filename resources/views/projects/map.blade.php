0e3e2f20bfb922c6a2249da18706d2ac647aff11
David <david.gruian@gmail.com>
2022-08-28 23:43:47 +0300

    added projects template

---
+++ resources/views/projects/map.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.projects')
+
+@section('js')
+    <script>
+        console.log('it works')
+    </script>
+@endsection
+
+@section('css')
+    body{
+        color: red;
+    }
+@endsection
+
+@section('content')
+    <h5>Some content here</h5>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/projects/{id}', function ($id) {
+    if (view()->exists('projects.'.$id)){
+        return view('projects.'.$id);
+    }
+    return redirect('/');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
